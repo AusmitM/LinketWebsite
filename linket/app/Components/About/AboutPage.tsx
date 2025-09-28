f53f97762656c1a861bb6260d95bdd727592851a
AusmitM <ausmitm715@gmail.com>
2025-09-28 18:48:07 -0500

    updating structure of linket website

---
+++ linket/app/Components/About/AboutPage.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useRouter } from "next/router";
+
+const AboutPage: React.FC = () => {
+  const router = useRouter();
+  return <div>{/* Landing page content goes here */}</div>;
+};
+
+export default AboutPage;
---
+++ linket/app/Components/Order/OrderPage.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useRouter } from "next/router";
+
+const OrderPage: React.FC = () => {
+  const router = useRouter();
+  return <div>{/* Order page content goes here */}</div>;
+};
+
+export default OrderPage;
